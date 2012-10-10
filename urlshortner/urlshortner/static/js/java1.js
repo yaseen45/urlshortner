e07807885ebdd6c93f8f2d03bb091d7a1bc0cec6
yaseen45 <yaseen@trialx.com>
2012-10-10 17:53:27 +0530

    "misssing /"

--- urlshortner/urlshortner/static/js/java1.js
+++ urlshortner/urlshortner/static/js/java1.js
@@ -1,18 +1,17 @@
 function check(){
-var luu=document.getElementById('lurl').value
-if(luu==''){
-document.getElementById("err").innerHTML="\"nothing entered\""
-    return false
+  
+    var luu=document.getElementById('lurl').value
+    if(luu==''){
+       document.getElementById("err").innerHTML="\"nothing entered\""
+       return false
     }
-
-var x=/^(http|https):\/\/
-if(x.test(luu)){
-      return true
-                  }
-else{
-    document.getElementById("err").innerHTML="\"wrong url\""
-    return false
+    var x=/^(http|https):\/\//
+    if(x.test(luu)){
+       return true
+    }
+   else{
+      document.getElementById("err").innerHTML="\"wrong url\""
+      return false
    }
-   
-   
+      
 }
