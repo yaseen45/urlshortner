07286f350b82c9f54aa3f8102e7fb874a454dbe4
yaseen45 <yaseen@trialx.com>
2012-10-11 11:52:34 +0530

    "added comments to the functions"

--- urlshortner/urlshortner/apps/shortner/tests.py
+++ urlshortner/urlshortner/apps/shortner/tests.py
@@ -12,38 +12,48 @@ class FirstTest(unittest.TestCase):
     
     def setUp(self):
         self.client = Client()
-
-       
-    def test_detail1(self):             #test to check which template is being rendered
-        res = Client()
-        p = res.get('')
-        print p.template.name
-        self.assertEqual(p.status_code, 200)
            
-    def test_detail2(self):
-        res = Client()
-        p = res.get('/assign/')
-        print p.template[0].name
-        print p.template[1].name
-        self.assertEqual(p.status_code, 200)
+    def test_renderbase(self):        
+        """this test check which templates is being rendered 
+         on the base url
+        """ 
+        request = Client()
+        response = request.get('')
+        print response.template.name
+        self.assertEqual(response.status_code, 200)
+           
+    def test_rendersign(self):
+        """this test also checks the templates which are 
+        being rendered at '/assign/'(url)
+        """         
+        request = Client()
+        response = request.get('/assign/')
+        print response.template[0].name
+        print response.template[1].name
+        self.assertEqual(response.status_code, 200)
     
-    def test_detail3(self):             #test to check '/admin/' is valid url
-        res = Client()
-        p = res.get('/admin/')
-        self.assertEqual(p.status_code, 200) 
+    def test_validd(self):             
+        """this test checks wheather '/admin/' is a valid url """
+        request = Client()
+        response = request.get('/admin/')
+        self.assertEqual(response.status_code, 200) 
    
     
     
-    def test_detail4(self):       #test to check that key is redirected to desired url & wrong key is redirected to 404
-        x = Ushort()
-        x.longurl = "http://www.yahoo.com/cricketlive"
-        x.shorturl = "cf1"
-        x.save()
-        p = self.client.get('/cf1/')
-        self.assertEqual(p.status_code,302) 
-        p = self.client.get('/fi89o/')
-        print p.status_code
-        self.assertEqual(p.status_code, 404) 
+    def test_redirects(self):
+        """"this test checks valid short url is redirected to
+        the desired url and invalid short url is redirected 
+        to 404"""
+        newobj = Ushort()
+        newobj.longurl = "http://www.yahoo.com/cricketlive"
+        newobj.shorturl = "cf1"
+        newobj.save()
+        response = self.client.get('/cf1/')
+        self.assertEqual(response.status_code,302) 
+        #now test for invalid url
+        response = self.client.get('/fi89o/')
+        print response.status_code
+        self.assertEqual(response.status_code, 404) 
              
     
         
--- urlshortner/urlshortner/apps/shortner/views.py
+++ urlshortner/urlshortner/apps/shortner/views.py
@@ -4,21 +4,33 @@ from django.shortcuts import render_to_response, get_object_or_404
 import random
 from models import Ushort
 
-RNG = int('ffff',16)   #range of value to be used                                
+RNG = int('ffff',16)   #range of short url to be used                                
 
 def index(request,templt='form.html'):
+    """
+    this is the function which is called on the base url
+    """
     return render_to_response(templt,context_instance=RequestContext(request))
 
 def search_assign(request,templt='output.html'):
+    """
+    this is the main function  which assigns short urls .
+    it is called when the valid long url is post by the user ,
+    this function checks if the long url is already shortned, 
+    if not it just creates a short url, store it and returns
+    it to the user
+    """ 
     if request.method=='POST':
         data = request.POST.copy()
         longu = data.get('lurl')
         contxt = {}
         try:
+           #here it checks if the long url is already shortned
            isthere = Ushort.objects.get(longurl=longu)
            contxt['shortu'] = isthere
            return render_to_response(templt,contxt,context_instance=RequestContext(request))
         except:
+           #here the code makes and assigns shortned url  
            newobj = Ushort()
            newobj.longurl = longu
            newobj.shorturl = hex(random.randrange(0,RNG,3))[2:]
@@ -28,6 +40,9 @@ def search_assign(request,templt='output.html'):
     return render_to_response('shortt.html',{},context_instance=RequestContext(request))
 
 def goto(request, shortt):
+    """this function redirects the  valid short urls to desired urls
+     and wrong ones to 404 
+    """
     isthere = get_object_or_404(Ushort,shorturl=shortt)
     yas = isthere.longurl
     return HttpResponseRedirect(yas)
