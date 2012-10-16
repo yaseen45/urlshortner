68380a04a73b04fbb5f7c6ca155e8b6c676038f1
yaseen45 <yaseen@trialx.com>
2012-10-16 23:08:01 +0530

    "changed the positon of js code"

--- urlshortner/urlshortner/static/js/java1.js
+++ urlshortner/urlshortner/static/js/java1.js
@@ -7,15 +7,13 @@ function check(){
     }
     var x=/^(http|https):\/\//
     var z=/^http:\/\/178\.79\.157\.230/
-     
-    if (luu.length<=20||z.test(luu)){
-       document.getElementById("err").innerHTML="\"already shortened\""
-      return false
-    }
+        
     if(x.test(luu)){
+        if (luu.length<=20||z.test(luu)){
+          document.getElementById("err").innerHTML="\"already shortened\""
+          return false
+         }   
       return true
-      
-    
     }
    else{
       document.getElementById("err").innerHTML="\"wrong url\""
