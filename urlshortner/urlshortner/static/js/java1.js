7f79d9b13d2f0c423a2dda63fb37e803bdd065b7
yaseen45 <yaseen@trialx.com>
2012-10-16 17:34:43 +0530

    "comments added"

--- urlshortner/urlshortner/static/js/java1.js
+++ urlshortner/urlshortner/static/js/java1.js
@@ -6,18 +6,16 @@ function check(){
        return false
     }
     var x=/^(http|https):\/\//
-    var z=/^http:\/\/((178\.79\.157\.230)|(bit\.ly)|(goo\.gl)|(ow\.ly))/
-    var y=/^http:\/\/((localhost)|(127\.0\.0\.1))/
-    if (y.test(luu)){
-      document.getElementById("err").innerHTML="\"oops localhost\""
+    var z=/^http:\/\/178\.79\.157\.230/
+     
+    if (luu.length<=20||z.test(luu)){
+       document.getElementById("err").innerHTML="\"already shortened\""
       return false
     }
     if(x.test(luu)){
-      if(z.test(luu)){
-       document.getElementById("err").innerHTML="\"already shortened\""
-      return false
-       }
       return true
+      
+    
     }
    else{
       document.getElementById("err").innerHTML="\"wrong url\""
